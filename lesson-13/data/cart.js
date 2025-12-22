28339107789eac5d000a301f730880875cf565eb
Mansi-Raj <mansi.raj.jnv@gmail.com>
2025-12-22 08:31:28 +0530

    lesson 15 orderSummary and paymentSummary

--- lesson-13/data/cart.js
+++ lesson-13/data/cart.js
@@ -71,3 +71,17 @@ export function updateQuantity(productId, newQuantity) {
 
   saveToCart();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToCart();
+}
